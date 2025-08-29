f7419d5bd7ded6f19c00e35bcb065495134722f0
Akshansh <thebigolwiseone@gmail.com>
2025-08-29 20:22:08 +0530

    input and display update

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use std::env;
+use sdl2::keyboard::Keycode;
 
 mod font;
 mod cpu;
@@ -13,6 +15,29 @@ mod cpu;
 const SCALE: u32 = 12;
 const WINDOW_WIDTH  : u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT : u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRM : usize = 12;
+
+fn control_map(key : Keycode) -> Option<usize>{
+    match key{
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
+}
 
 fn draw(emu: &cpu::Processor, canvas: &mut Canvas<Window>){
     canvas.set_draw_color(Color::RGB(0,0,0));
@@ -35,6 +60,10 @@ fn draw(emu: &cpu::Processor, canvas: &mut Canvas<Window>){
 
 fn main() {
 
+    let mut args = env::args();
+    args.next();
+    let path = args.next().unwrap();
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
@@ -56,7 +85,7 @@ fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
     
     let mut chip = cpu::Processor::new();
-    let mut rom  = File::open("/home/candy/Downloads/imb.ch8").expect("Unable to open file");
+    let mut rom  = File::open(&path).expect("Unable to open file");
     let mut buffer = Vec::new();
     rom.read_to_end(&mut buffer).unwrap();
     chip.load_state(&buffer);
@@ -65,12 +94,26 @@ fn main() {
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit{..} => {
+                    chip.reset();
                     break 'gameloop;
+                },
+                Event::KeyDown{keycode : Some(key), ..} => {
+                    if let Some(k) = control_map(key){
+                        chip.key_input(k,true);
+                    }
+                },
+                Event::KeyUp{keycode : Some(key), ..} => {
+                    if let Some(k) = control_map(key){
+                        chip.key_input(k,false);
+                    }
                 },
                 _ => ()
             }
         }
-        chip.tick();
+        for _ in 0..TICKS_PER_FRM{
+            chip.tick();
+        }
+        chip.tick_timers();
         draw(&chip,&mut canvas);
     }
 }
